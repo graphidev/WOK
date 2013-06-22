06ddbd05377f1eefa379de43137abe8d3517ac77
Sébastien ALEXANDRE <sebastien@graphidev.fr>
2013-06-22 18:41:45 +0200

    Add get_library()

--- core/templates.php
+++ core/templates.php
@@ -1,8 +1,8 @@
 <?php
 
-	function tpl_static_page($page) {
-		if(file_exists(root(PATH_TEMPLATE."/$page.php"))):
-			include(root(PATH_TEMPLATE."/$page.php"));
+	function tpl_static_page($name) {
+		if(file_exists(root(PATH_TEMPLATE."/$name.php"))):
+			include(root(PATH_TEMPLATE."/$name.php"));
 		else:
 			include(root(PATH_TEMPLATE."/404.php"));
 		endif;
@@ -39,5 +39,40 @@
 			return false;
 		endif;
 	}
+
+    function get_library($name) {
+        
+        if(is_dir(root(PATH_LIBS."/$name")) && file_exists(root(PATH_LIBS."/$name/$name.xml"))):
+            
+            $xml = new DOMDocument();
+			$xml->load(root(PATH_LIBS."/$name/$name.xml"));
+            $library = $xml->getElementsByTagName('library')->item(0);
+            $files = $library->getElementsByTagName('files')->item(0)->getElementsByTagName('case');
+            
+            foreach($files as $i => $case) {
+                
+                $type = $case->getAttribute('type');
+                $path = $case->nodeValue;
+                
+                switch($type) {
+                    case 'css':
+                        echo '<link href="'.path(PATH_LIBS."/$name$path").'" rel="stylesheet" type="text/css">';
+                    break;
+                    case 'js':
+                        echo '<script type="text/javascript" src="'.path(PATH_LIBS."/$name$path").'"></script>';
+                    break;
+                    
+                    case 'php':
+                        include(root(PATH_LIBS."/$name$path"));
+                    break;
+                    
+                }
+            }
+                
+        else:
+            return false;
+        endif;
+        
+    }
 	
 ?>
--- template/homepage.php
+++ template/homepage.php
@@ -1,5 +1,10 @@
 <html>
-
+    
+    <head>
+        <title>This is the homepage</title>
+        <?php get_library('bootstrap'); ?>
+    </head>
+    
     <h1>Welcome on homepage</h1>
 
 </html>

cafd3737a919f315e86c886e44f66bd7404a9efd
Sébastien ALEXANDRE <sebastien@loungeup.com>
2013-09-30 21:25:06 +0200

    Add options to the cleaner script (-all, -tmp, -logs, -uploads)
    This script should be launched as super user

--- scripts/clean.php
+++ scripts/clean.php
@@ -6,13 +6,15 @@
     define('ACCESS_PATH', dirname(dirname(__FILE__)));
 
     require_once ACCESS_PATH . "/core/init.php";
+    
+    $options = $argv;
 
     function remove($tmp, $path) {
         
         foreach($tmp as $name => $value) {
             if(is_array($value)):
                 remove($value, "$path/$name");
-                $deleted = rmdir("$path/$name");
+                $deleted = @rmdir("$path/$name");
             else:
                 $deleted = @unlink("$path/$name");
             endif;
@@ -24,16 +26,75 @@
         }
     }
 
-    echo "\nCleaning ".PATH_TMP." ...\n\n";
+    function input($string) {
+        echo $string;
+        $handle = fopen("php://stdin","r");
+        $data = trim(fgets($handle));
+        if($data == 'exit')
+            exit;
+        else
+            return $data;
+    }
 
-    $tmp = tree(ACCESS_PATH.PATH_TMP);
+    echo "\n[WOK cleaner]\n";
 
-    if(!empty($tmp))
-        remove($tmp, ACCESS_PATH.PATH_TMP);
+    /**
+     * Ask for an option
+    **/
+    if(count($options) == 1):
+        $input = input("Which folders (separate with a space) ?  [all|(tmp|logs|uploads)] :");
+        $folders = explode(' ', $input);
+        foreach($folders as $i => $folder) {
+            $options[] = "-$folder";
+        }
+    endif;
     
-    else
-        exit("/!\ Temporary directory is still empty.\n\n");
+    /**
+     * Clean tmp folder
+    **/
+    if(in_array('-tmp', $options) || in_array('-all', $options)): 
+        $tmp = tree(ACCESS_PATH.PATH_TMP);
+    
+        if(!empty($tmp)):
+            echo "Clean ".PATH_TMP." :\n";
+            remove($tmp, ACCESS_PATH.PATH_TMP);
+            echo "\n\n";
+        endif;
+        
+    endif;
 
-    echo "\nDone !\n\n";
+    /**
+     * Clean tmp folder
+    **/
+    if(in_array('-logs', $options) || in_array('-all', $options)): 
+        
+        $tmp = tree(ACCESS_PATH.PATH_LOGS);
+    
+        if(!empty($tmp)):
+            echo "Clean ".PATH_LOGS." :\n";
+            remove($tmp, ACCESS_PATH.PATH_LOGS);
+            echo "\n\n";
+        endif;
+        
+        
+    endif;
+
+
+    /**
+     * Clean uploads folder
+    **/
+    if(in_array('-uploads', $options) || in_array('-all', $options)): 
+        
+        $tmp = tree(ACCESS_PATH.PATH_TMP_FILES);
+    
+        if(!empty($tmp)):
+            echo "Clean ".PATH_LOGS." :\n";
+            remove($tmp, ACCESS_PATH.PATH_TMP_FILES);
+            echo "\n\n";
+        endif;
+        
+    endif;
+
+    echo "Temporary folders cleaned\n";
 
 ?>

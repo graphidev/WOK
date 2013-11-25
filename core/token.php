8327c600c272da5e4901aa56ace5dc695a4000b7
Sébastien ALEXANDRE <sebastien@loungeup.com>
2013-11-25 15:08:39 +0100

    Implement Token class

---
+++ core/token.php
@@ -0,0 +1,54 @@
+<?php
+
+    class Token {
+        
+        /**
+         * Generate a token
+        **/
+        public static function generate($name) {
+            $token = uniqid(rand());
+			Session::set('tokens', array(
+                $name => array(
+                    'key' => $token,
+                    'time' => time()
+                )
+            ));
+			return $token;
+        }
+        
+        
+        /**
+         * Check token validity
+        **/
+        public static function authorized($name, $key, $lifetime) {
+            $token = self::_get($name);
+            if(!$token) return false;
+            
+            $elapsed = time() - $token['time'];
+            
+            return ($key == $token['key'] && $elapsed <= $lifetime) ? true : false;
+        }
+        
+        
+        /**
+         * Destroy token
+        **/
+        public static function destroy($name) {
+            
+        }
+        
+        /**
+         * Get token
+        **/
+        private static function _get($name) {
+            $tokens = Session::has('tokens') ? Session::get('tokens') : array();
+            
+            if(!empty($tokens[$name]))
+                return $tokens[$name];
+            else
+                return false;
+        }
+        
+    }
+
+?>
--- templates/homepage.php
+++ templates/homepage.php
@@ -1,5 +1,20 @@
 It works !
 
+<?php 
+    if(!empty($_POST['token'])):
+        if(Token::authorized('test', $_POST['token'], 2))
+            exit('Token authorized');
+        else
+            echo 'Not authorized token';
+    endif;
+?>
+
+<form action="#" method="post">
+    <input type="hidden" name="token" value="<?php echo Token::generate('test'); ?>">
+    <input type="submit">
+</form>
+
+<?php exit; ?>
 <?php Session::logout(); ?>
 <?php Session::login(null, true); ?>
 

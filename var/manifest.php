92960aff78c52a1d596c5ee4cad82b0a71c609b5
Sébastien ALEXANDRE <sebastien@loungeup.com>
2013-11-25 12:26:41 +0100

    Remove duplicated Locales::_e() shortcut

--- var/manifest.php
+++ var/manifest.php
@@ -5,13 +5,6 @@
      * It allow you to do anything you want before calling templates
     **/ 
 
-    /**
-     * Define Locale::_e shortcut
-    **/
-    function _e($locale, $data = array()) {
-        return Locales::_e($locale, $data);
-    }
-
 
     
 ?>

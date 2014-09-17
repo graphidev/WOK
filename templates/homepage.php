e0e601e6230de501cd343248a7de6630fce6d849
Sébastien ALEXANDRE <sebastien@graphidev.fr>
2014-09-17 14:21:37 +0200

    Updgrade default templates

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,3 +1,5 @@
 <?php
-    echo Markdown::defaultTransform(file_get_contents(SYSTEM_ROOT."/README.md"));
+    echo Parsedown::instance()->text(file_get_contents(SYSTEM_ROOT."/README.md"));
+
+    //Markdown::defaultTransform(file_get_contents(SYSTEM_ROOT."/README.md"));
 ?>

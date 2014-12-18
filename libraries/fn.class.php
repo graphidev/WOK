a95e5eeedc892dbcd2ed44226ed69cb0d82b54b4
Sébastien ALEXANDRE <Sebastien@MacBook-Pro-de-Sebastien.local>
2014-12-18 21:26:40 +0100

    Experimental : function autoloader
    This class could replace the load_library() helper function

---
+++ libraries/fn.class.php
@@ -0,0 +1,39 @@
+<?php
+
+    /**
+     * Functions autoloader that allow you to get 
+     * functions access without calling file
+    **/
+    class Fn {
+        
+        /**
+         * Load a defined library
+         * @param string $library   The library name. Library files must have the following syntax : [name].library.php
+        **/
+        public static function load($library) {
+            
+            if(!file_exists($path = PATH_LIBRARIES .'/'. $library . '.library.php'))
+                trigger_error('Call to undefined library '. $library .' within '. PATH_LIBRARIES, E_USER_ERROR);
+            
+            require_once $path;
+        }
+        
+        /**
+         * Call of a function : try to load function with a defined file name
+         * Please note that `load` function name is reserved by the class for libraries loading
+         * Furthermore, this could be used exceptionaly in order to keep system performance
+        **/
+        public static function __callStatic($function, $arguments) {
+            
+            if(!function_exists($function) && file_exists($path = PATH_LIBRARIES .'/'. $function . '.function.php')) {
+                require_once $path;
+            }
+            
+            if(!function_exists($function))
+                trigger_error('Call to undefined function '.$function.'()', E_USER_ERROR);
+            
+            return call_user_func_array($function, $arguments);
+            
+        }
+        
+    }

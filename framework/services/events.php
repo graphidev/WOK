79a4da7cf4d98541b60ea1c42d6b9c4b634f03fb
graphidev <sebastien@graphidev.fr>
2015-03-30 20:43:12 +0200

    Allow multiple event callbacks

--- framework/services/events.php
+++ framework/services/events.php
@@ -11,26 +11,53 @@
 
     namespace Framework\Services;
 
+
+    /**
+     * Allow to define and call events
+     * during the run time process.
+    **/
     class Events {
 
+        /**
+         * @var array $events      Events calback collection
+        **/
         private $events     = array();
 
-        public function register($name, $callback) {
-            $this->events[$name] = $callback;
+        /**
+         * Register a callback to a given event
+         * @param   string    $name         Event name
+         * @param   Closure   $callaback    Event callback
+        **/
+        public function register($name, \Closure $callback) {
+            $this->events[$name][] = $callback;
         }
 
+
+        /**
+         * Fire an event
+         * @param   string      $name           Event name
+         * @param   array       $parameters     Callback parameters
+        **/
+        public function trigger($name, array &$parameters = array()) {
+
+            if(empty($this->events[$name]))
+                return;
+
+            foreach($this->events[$name] as $event) {
+                call_user_func_array($this->events[$name], $parameters);
+            }
+
+        }
+
+
+        /**
+         * Remove an event listener
+         * @param string   $name    Event name
+        **/
         public function remove($name) {
+
             if(isset($this->events[$name]))
-
-            unset($this->events[$name]);
-        }
-
-        public function trigger($name, $parameters = array()) {
-
-            if(!isset($this->events[$name]))
-                trigger_error('Undefined event '.$name.' callback', E_USER_ERROR);
-
-            call_user_func_array($this->events[$name], $parameters);
+                unset($this->events[$name]);
 
         }
 
